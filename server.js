91b66f10dd0d805fa8c69797f95209a72b51868b
Lee Graham <graham.la3@gmail.com>
2018-10-10 14:42:42 -0600

    Setup endpoints and remove duplicates from star data

--- server.js
+++ server.js
@@ -1,11 +1,40 @@
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 
+app.set('port', process.env.PORT || 3000);
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.locals.title = 'Star-Finder';
+
+
 app.get('/', (request, response) => {
-  response.send('hello world');
+  response.send(app.locals.title);
 });
 
-// app.use(express.static('public'))
+app.get('/api/v1/stars', (request, response) => {
+  database('stars').select()
+    .then((stars) => {
+      response.status(200).json(stars);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    });
+});
+
+app.get('/api/v1/exoplanets', (request, response) => {
+  database('exoplanets').select()
+    .then((exoplanets) => {
+      response.status(200).json(exoplanets);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    });
+});
 
 app.listen(3000, () => {
   console.log('Express intro running on localhost:3000');
