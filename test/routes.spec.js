0d6ce991ef73cdb001c0e8f7a185ce4570eb495f
Jesse McFadden <jessemcbrennan@gmail.com>
2018-10-13 14:54:52 -0600

    Adjust tests for travis ci

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -47,9 +47,7 @@ describe('API Routes', () => {
         response.body.should.be.a('array');
         response.body.length.should.equal(30);
         response.body[0].should.have.property('name');
-        // response.body[0].name.should.equal('OGLE-2016-BLG-1469L');
         response.body[0].should.have.property('mass');
-        // response.body[0].mass.should.equal('271.945750016');
         done();
       });
     });
@@ -63,9 +61,7 @@ describe('API Routes', () => {
         response.should.be.json;
         response.body.should.be.a('array');
         response.body.length.should.equal(840);
-        // response.body[0].should.have.property('name');
         response.body[0].name.should.equal('OGLE-2016-BLG-1469L b');
-        // response.body[0].mass.should.equal('13.6');
         done();
       });
     });
@@ -133,7 +129,4 @@ describe('API Routes', () => {
       })
     })
   });
-  // describe('DELETE /api/v1/stars/:id', (request, response) => {
-  //   chais.request(app)
-  // })
 });
