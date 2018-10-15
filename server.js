3984f87f17822df7e398b195a06f80c992fe683f
Jesse McFadden <jessemcbrennan@gmail.com>
2018-10-15 16:20:05 -0600

    Fixing local merge conflict

--- server.js
+++ server.js
@@ -6,25 +6,45 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const name = '11 Com'
+const cors = require('cors');
 
-app.set('port', process.env.PORT || 3000);
+app.use(cors())
+app.set('port', process.env.PORT || 3009);
 app.use(bodyParser.json());
+
 // app.use(express.static('public/'));
-app.locals.title = 'Star-Finder';
+app.locals.title = 'Star-Finder is deploying successfully';
+
 
 
 app.get('/', (request, response) => {
-  response.send('Youre doing it Peter');
+  response.status(200).json(app.locals.title);
 });
 
 app.get('/api/v1/stars', (request, response) => {
-  database('stars').select()
+  if(request.query.deletable) {
+    let stars_deletable = request.query.deletable.toLowerCase()
+ 
+  database('stars')
+    .where({deletable: stars_deletable })
+    .select()
     .then((stars) => {
-      response.status(200).json(stars);
+      response.status(
+        200).json(stars);
     })
     .catch((error) => {
       response.status(500).json({ error });
-    });
+    }); 
+  } else {
+    database('stars')
+      .select()
+      .then(stars => {
+        response.status(200).json(stars)
+      })
+      .catch(error => {
+        response.status(500).json({error})
+      })
+  }
 });
 
 
@@ -53,6 +73,32 @@ app.get('/api/v1/stars/:id', (request, response) => {
     response.status(500).json({ error })
   })
 })
+
+app.get('/api/v1/stars', (request, response) => {
+  if(request.query) {
+    let star = request.body
+
+    database('stars')
+      .where({ star })
+      .select()
+      .then(stars => {
+        response.status(200).jason(stars)
+      })
+      .catch(error => {
+        response.status(500).json({ error })
+      });
+  } else {
+      database('stars')
+      .select()
+      .then(stars => {
+        response.status(200).json(stars)
+      })
+      .catch(error => {
+        response.status(500).json({error})
+      })
+  }
+})
+
 
 // app.get('/api/v1/stars', (request, response) => {
 //   database('stars').where('id', request.params.id).select()
@@ -174,3 +220,5 @@ app.listen(app.get('port'), () => {
 });
 
 module.exports = {app, database};
+
+//here are some changes
